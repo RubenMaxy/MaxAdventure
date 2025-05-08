a2afedc3c97c7d0e56e6e78a22eef2af64b164e4
Rubén <151085484+RubenMaxy@users.noreply.github.com>
2025-05-08 21:50:54 +0200

    Seguimiento de la cámara.

---
+++ Max Adventure/Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SeguimientoCamara : MonoBehaviour
+{
+    public Transform player; // El objeto Transform del jugador
+    public float offset = 0; // La distancia de la cámara al jugador. Para que aparezca mas a la derecha o izquierda en la pantalla el Player
+
+    private void LateUpdate() // Se ejecuta después de la actualización principal, para evitar problemas de movimiento
+    {
+        if (player != null)
+        {
+            // Calcula la nueva posición de la cámara
+            Vector3 newPosition = new Vector3(player.position.x + offset, transform.position.y, transform.position.z);
+
+            // Actualiza la posición de la cámara
+            transform.position = newPosition;
+        }
+    }
+}
